21b78f82a991820db127a3ac47077f31bc6f3468
Ryan OConnor <ryanoconnor010@gmail.com>
2021-03-13 16:35:24 -0500

    PingServer methods all updated and work

--- src/com/company/PingServer.java
+++ src/com/company/PingServer.java
@@ -1,95 +1,64 @@
+// Ryan OConnor
+// Computer Networks - Monday/Wednesday class
+// Project 1 - UDP Ping Client/AServer program
+// due: 3/13/21 @ 23:59
+
 package com.company;
-import java.io.*;
 import java.net.*;
 import java.util.*;
 
-/*
- * Server to process ping requests over UDP.
- */
+
 public class PingServer
 {
-    private static final double LOSS_RATE = 0.3;
-    private static final int AVERAGE_DELAY = 100;  // milliseconds
-
     public static void main(String[] args) throws Exception
     {
-        // Get command line argument.
-        if (args.length != 1) {
-            System.out.println("Required arguments: port");
-            return;
-        }
-        int port = Integer.parseInt(args[0]);
-
-        
-
-        // Create random number generator for use in simulating
-        // packet loss and network delay.
-        Random random = new Random();
-
-        // Create a datagram socket for receiving and sending UDP packets
-        // through the port specified on the command line.
-        DatagramSocket socket = new DatagramSocket(port);
+        int my_port = 2014;
+        DatagramSocket serverSocket = new DatagramSocket(my_port);
+        byte[] receiveData = new byte[1024];
 
         // Processing loop.
         while (true) {
-            // Create a datagram packet to hold incomming UDP packet.
-            DatagramPacket request = new DatagramPacket(new byte[1024], 1024);
 
-            // Block until the host receives a UDP packet.
-            socket.receive(request);
+            // Create a random number generator for use in packet loss simulation
+            Random random = new Random();
 
-            // Print the recieved data.
-            printData(request);
+            // Create a datagram packet to hold incoming UDP packet.
+            DatagramPacket receivePacket = new DatagramPacket(receiveData, receiveData.length);
 
-            // Decide whether to reply, or simulate packet loss.
-            if (random.nextDouble() < LOSS_RATE) {
-                System.out.println("   Reply not sent.");
+            // Get the client message (wait's for client to send packet)
+            serverSocket.receive(receivePacket);
+
+
+            String sentence = new String(receivePacket.getData(),0, receivePacket.getLength());
+            InetAddress IPAddress = receivePacket.getAddress();
+            int client_port = receivePacket.getPort();
+
+            //Print out client's IP Address, port number and the message
+            System.out.println("Client's port # = " + client_port);
+            System.out.println("Client's IP address = " + IPAddress);
+            System.out.println("Client's message = " + sentence);
+
+            // Capitalize the message from the client
+            String capitalizedSentence = sentence.toUpperCase();
+
+            //Simulate the packet loss
+            int rand = random.nextInt(10); // a random number in the range of 0-10
+            System.out.print("Random number chosen was: " + rand);
+
+            // If rand is less than 4, we consider that packet lost and do not reply
+            if (rand < 4) {
+                System.out.println("Reply not sent");
                 continue;
             }
 
-            // Simulate network delay.
-            Thread.sleep((int) (random.nextDouble() * 2 * AVERAGE_DELAY));
+            //Otherwise, this server will respond - IPAddress is the client's host IP
+            byte[] sendData = capitalizedSentence.getBytes();
+            DatagramPacket sendPacket = new DatagramPacket(sendData, sendData.length, IPAddress, client_port);
+            serverSocket.send(sendPacket);
+            System.out.println("\nReply to the client sent");
 
-            // Send reply.
-            InetAddress clientHost = request.getAddress();
-            int clientPort = request.getPort();
-            byte[] buf = request.getData();
-            DatagramPacket reply = new DatagramPacket(buf, buf.length, clientHost, clientPort);
-            socket.send(reply);
 
-            System.out.println("   Reply sent.");
+
         }
-    }
-
-    /*
-     * Print ping data to the standard output stream.
-     */
-    private static void printData(DatagramPacket request) throws Exception
-    {
-        // Obtain references to the packet's array of bytes.
-        byte[] buf = request.getData();
-
-        // Wrap the bytes in a byte array input stream,
-        // so that you can read the data as a stream of bytes.
-        ByteArrayInputStream bais = new ByteArrayInputStream(buf);
-
-        // Wrap the byte array output stream in an input stream reader,
-        // so you can read the data as a stream of characters.
-        InputStreamReader isr = new InputStreamReader(bais);
-
-        // Wrap the input stream reader in a bufferred reader,
-        // so you can read the character data a line at a time.
-        // (A line is a sequence of chars terminated by any combination of \r and \n.)
-        BufferedReader br = new BufferedReader(isr);
-
-        // The message data is contained in a single line, so read this line.
-        String line = br.readLine();
-
-        // Print host address and data received from it.
-        System.out.println(
-                "Received from " +
-                        request.getAddress().getHostAddress() +
-                        ": " +
-                        new String(line) );
     }
 }
